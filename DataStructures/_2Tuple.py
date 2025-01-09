08ae4757ce7a60dfa6dde60ea70c8ff844c047db
Eray İnal <114001362+erayinal@users.noreply.github.com>
2025-01-09 21:18:49 +0300

    Initial commit

---
+++ DataStructures/_2Tuple.py
@@ -0,0 +1,11 @@
+
+# Listeler içerisine birden fazla türden değişken alabiliyordu, sıralıydı ve eğiştirilebilirdi
+# Tuple'ın en önemli farklılığı DEĞİŞTİRİLEMEZ olmasıdır
+
+# Yine list'lerdeki gibi iki farlı oluşturma yolu vardır:
+    #1: () ile -> ("ali", "veli", 1, 3)
+    #2: virgül ile -> "ali", "veli", 1, 3
+    
+
+t = ("ali", "veli")
+
---
+++ Python1.py
@@ -0,0 +1,24 @@
+
+
+str = "eray inal 23 yaşına girecek"
+str2 = str.strip("e")   #Başında veya sonunda e harfi varsa kaldırır
+
+
+# int1 = input()  # input alır 
+
+
+# print("eray", "inal", "23", sep="_")  #İfadeler arasındaki seperator'ı belirler
+
+
+ifade = "geleceği yazıyoruz"
+ifa = ifade[0:2]
+
+
+leng = len(ifade)
+
+
+
+
+
+
+
