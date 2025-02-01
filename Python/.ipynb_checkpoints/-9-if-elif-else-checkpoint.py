b41437cc54e80a5534bb90fb4fad39b253dfffc0
Eray İnal <114001362+erayinal@users.noreply.github.com>
2025-02-01 20:32:33 +0300

    Updated Seaborn section for data visualization

---
+++ Python/.ipynb_checkpoints/-9-if-elif-else-checkpoint.py
@@ -0,0 +1,125 @@
+
+"""
+yaş = int(input("yaşınız: "))
+
+
+
+
+if(yaş>=18):
+    print("Girebilir")
+else:
+    print("Giremez")
+
+"""
+
+
+
+
+
+
+
+"""
+score = float(input("Kaç aldın: "))
+
+
+if(score>=90):
+    print("AA aldınız")
+
+elif(score>=80):
+    print("BA aldın")
+elif(score>=70):
+    print("BB aldınız")
+
+
+else:
+    print("Knocked up of class/Kaldın")
+
+"""
+
+
+
+
+
+
+
+
+"""
+V = float(input("Anlık hız: "))
+r = float(input("Yarıçap of road: "))
+m = float(input("kütle of car: "))
+g = 9.8
+k = 0.8
+
+Fs = float(k * m * g)
+Fm = float(m * V**2 / r)
+
+if(Fs>Fm):
+    print("GÜvendesin...")
+
+else:
+    print("yarra yedin...")
+
+"""
+
+
+
+
+
+
+
+"""
+a = True
+
+b = not a      #Bunu yaparak "true" olan a'yı tersine çevirdik.
+print(b)
+
+"""
+
+
+
+
+
+
+
+
+
+
+"""
+kullanıcı_adı = "erayinl"
+şifre = "eray"
+
+
+a = input("Kullanıcı Adı: ")
+b = input("Şifre: ")
+
+
+if(a == kullanıcı_adı and (b == şifre )):
+    print("Login Succesfull...")
+
+
+elif(a != kullanıcı_adı and b == şifre):
+    print("User Was't Found...")
+elif((a == kullanıcı_adı ) and (b != şifre)):
+    print("Wrong Password...")
+
+
+else:
+    print("Try Again...")
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
